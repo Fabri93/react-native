fdea91e9444d21c7aae6e0cbab3ba8fc5c78c627
Fabrzio Lasaponara <fabrzio.lasaponara@hotmail.it>
2018-10-20 00:42:12 +0200

    Activity Indicator

--- confusion/components/HomeComponent.js
+++ confusion/components/HomeComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -16,7 +17,21 @@ const mapStateToProps = state => {
 function RenderItem(props) {
     
     const item = props.item;
-    
+
+    if (props.isLoading) {
+        return(
+                <Loading />
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <View> 
+                <Text>{props.erreMess}</Text>
+            </View>
+        );
+    }
+
+    else{
     if (item != null) {
         return(
             <Card
@@ -33,6 +48,7 @@ function RenderItem(props) {
         return(<View></View>);
     }
 }
+}
 
 class Home extends Component {
 
@@ -48,9 +64,18 @@ render() {
     
     return(
         <ScrollView>
-            <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} />
-            <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} />
-            <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} />
+            <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+            isLoading={this.props.dishes.isLoading}
+            erreMess={this.props.dishes.erreMess}  
+            />
+            <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+            isLoading={this.props.promotions.isLoading}
+            erreMess={this.props.promotions.erreMess}
+             />
+            <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+             isLoading={this.props.leaders.isLoading}
+             erreMess={this.props.leaders.erreMess} 
+             />
         </ScrollView>
     );
 }
--- confusion/components/MenuComponent.js
+++ confusion/components/MenuComponent.js
@@ -3,6 +3,7 @@ import { View, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -40,6 +41,18 @@ class Menu extends Component  {
             );
         };
         
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
 
         return (
             <FlatList 
